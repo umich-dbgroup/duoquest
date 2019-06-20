ce1a647f38fe4a71600302729169c18facfba295
Chris Baik <cjbaik@umich.edu>
2019-06-19 21:44:43 -0400

    Remove b and n parameters

--- src/main.py
+++ src/main.py
@@ -6,10 +6,6 @@ def main():
     parser.add_argument('dataset', choices=['spider', 'wikisql'])
     parser.add_argument('mode', choices=['dev', 'test'])
     parser.add_argument('--mixtape', action='store_true', help='Enable Mixtape')
-    parser.add_argument('--b', default=5, type=int,
-        help='Beam search parameter')
-    parser.add_argument('--n', default=5, type=int,
-        help='Max number of final queries to output')
     parser.add_argument('--cache', action='store_true', help='Enable cache')
     parser.add_argument('--batch', action='store_true', help='Enable batching')
     args = parser.parse_args()
