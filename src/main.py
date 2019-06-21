d50a077ee77866a674d48abd44151034e18e7e8b
Chris Baik <cjbaik@umich.edu>
2019-06-21 14:27:36 -0400

    Update to run tasks

--- src/main.py
+++ src/main.py
@@ -1,17 +1,57 @@
 import argparse
+import configparser
+import json
+
+from modules.client import TaskClient
+from proto.task_pb2 import ProtoTask, ProtoCandidates
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('system', choices=['syntaxsql'])
     parser.add_argument('dataset', choices=['spider', 'wikisql'])
     parser.add_argument('mode', choices=['dev', 'test'])
+    parser.add_argument('--out_path', default='mixtape.sqls')
+
+    # Server args
+    parser.add_argument('--port', default=6000)
+    parser.add_argument('--authkey', default=b'mixtape')
+
+    # Enabling/disabling features
     parser.add_argument('--mixtape', action='store_true', help='Enable Mixtape')
+
+    # TODO
     parser.add_argument('--cache', action='store_true', help='Enable cache')
     parser.add_argument('--batch', action='store_true', help='Enable batching')
+
     args = parser.parse_args()
 
-    # TODO: ensure system server is running
-    # TODO: load dataset
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    # Load dataset
+    data = None
+    if args.dataset == 'spider':
+        if args.mode == 'dev':
+            data = json.load(open(config['spider']['dev_path']))
+        elif args.mode == 'test':
+            data = json.load(open(config['spider']['test_path']))
+    elif args.dataset == 'wikisql':
+        # TODO
+        pass
+
+    # Run each task
+    if args.mixtape:
+        # TODO if activated
+        pass
+    else:
+        client = TaskClient()
+        f = open(args.out_path, 'w+')
+        for task in data:
+            cqs = client.run(task['db_id'], task['question'])
+            f.write(u'\t'.join(cqs))
+            f.write('\n')
+        f.close()
+        client.close()
 
 if __name__ == '__main__':
     main()
---
+++ src/modules/client.py
@@ -0,0 +1,26 @@
+from multiprocessing.connection import Client
+
+from ..proto.task_pb2 import ProtoTask, ProtoCandidates
+
+class TaskClient:
+    def __init__(self, port, authkey):
+        self.port = port
+        self.authkey = authkey
+
+    def connect(self):
+        address = ('localhost', self.port)
+        self.conn = Client(address, authkey=self.authkey)
+
+    def run(self, db_name, nlq):
+        task = ProtoTask()
+        task.db_name = db_name
+        task.nlq = nlq
+
+        self.conn.send_bytes(task)
+        msg = self.conn.recv_bytes()
+        sqls = ProtoCandidates.ParseFromString(msg)
+        return sqls
+
+    def close(self):
+        self.conn.send_bytes(b'close')
+        self.conn.close()
