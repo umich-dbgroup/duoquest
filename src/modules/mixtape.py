64d78e6d69a20e795a7f09551030d2f7dbcfc72d
Chris Baik <cjbaik@umich.edu>
2019-06-29 11:11:06 -0400

    call types correctly in mixtape

--- src/modules/mixtape.py
+++ src/modules/mixtape.py
@@ -17,11 +17,11 @@ class Mixtape:
                 if agg_col.agg in (COUNT, SUM) and tsq_type != 'number':
                     return Tribool(False)
                 # min/max/avg can never produce a string
-                if agg_col.agg in (MIN, MAX, AVG) and tsq_type == 'string':
+                if agg_col.agg in (MIN, MAX, AVG) and tsq_type == 'text':
                     return Tribool(False)
             elif agg_col.has_agg == UNKNOWN:
                 # no agg func can convert num -> str
-                if col_type == 'number' and tsq_type == 'string':
+                if col_type == 'number' and tsq_type == 'text':
                     return Tribool(False)
             else:
                 # col type must match tsq type
@@ -43,7 +43,24 @@ class Mixtape:
 
         return None
 
-    def prune_by_row(self, query, tsq):
+    def ready_for_row_check(self, query):
+        # TODO: what happens when there are set ops?
+
+        # If there MIGHT be an aggregate anywhere and
+        #   WHERE or GROUP BY is incomplete
+        if any(map(lambda x: x.has_agg != FALSE, query.select)):
+            if query.has_where == UNKNOWN or \
+                (query.has_where == TRUE and not query.where.predicates):
+                return False
+
+            if query.has_group_by == UNKNOWN or \
+                (query.has_group_by == TRUE and not query.group_by):
+                return False
+
+        return True
+
+    def prune_by_row(self, db, schema, query, tsq):
+        # TODO
         pass
 
     def prune_by_num_cols(self, query, tsq):
@@ -55,45 +72,20 @@ class Mixtape:
             return None              # nothing to prune
 
     def verify(self, db, schema, query, tsq):
-        # check types
-        if query.select:
-            check_num_cols = self.prune_by_num_cols(query, tsq)
-            if check_num_cols is not None:
-                return check_num_cols
+        if not query.select:
+            return Tribool(None)    # hasn't even started with select
 
-            for i, aggcol in enumerate(query.select):
-                check_col = self.prune_select_column(db, schema, aggcol, tsq, i)
-                if check_col is not None:
-                    return check_col
+        check_num_cols = self.prune_by_num_cols(query, tsq)
+        if check_num_cols is not None:
+            return check_num_cols
 
-        return Tribool(None)        # return indeterminate
-
-        # if not query.joinpath:
-            # TODO: calculate join path
-            # pass
-
-        # stores tsq positions fulfilling each proj
-        # proj_tsq_poses = []
-
-        # TODO: check projs size and tsq cols size
-        # TODO: check projs types and tsq cols types
-
-        # for proj in query.projs():
-            # TODO: returns Tribool result and tsq positions fulfilling it
-            # result, tsq_poses = self.prune_by_column(proj, tsq)
-
-            # if not result.value:
-                # TODO: cache bad projection somehow
-                # return Tribool(False)
-
-        # TODO: check each permutation of proj ordering matching tsqs
-        # for each valid permutation:
-            # TODO: reorder query projs, then execute
-            # self.prune_by_row(query, tsq)
+        for i, aggcol in enumerate(query.select):
+            check_col = self.prune_select_column(db, schema, aggcol, tsq, i)
+            if check_col is not None:
+                return check_col
 
         # TODO
-        # if query is finished:
-            # run query and check ordering, flip if necessary
+        # if self.ready_for_row_check(query):
+        #     self.prune_by_row(db, schema, query, tsq)
 
-        # TODO: T, F, or None for indeterminate goes in constructor
-        # return Tribool()
+        return Tribool(None)        # return indeterminate
