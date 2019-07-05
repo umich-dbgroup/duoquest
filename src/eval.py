3b0b1ef10f0bed1762b16ec47c8803c26de454cd
Chris Baik <cjbaik@umich.edu>
2019-07-05 15:40:31 -0400

    make default b None

--- src/eval.py
+++ src/eval.py
@@ -948,7 +948,7 @@ if __name__ == "__main__":
     # NLQ parameters
     parser.add_argument('--n', default=10, type=int,
         help='Max number of final queries to output')
-    parser.add_argument('--b', default=1, type=int,
+    parser.add_argument('--b', default=None, type=int,
         help='Beam search parameter')
 
     # TODO
