7be977dbdefca12a038e3dcfb0a2b44a126c2de6
Chris Baik <cjbaik@umich.edu>
2019-07-17 20:52:38 -0400

    limit produced subqs

--- duoquest/verifier.py
+++ duoquest/verifier.py
@@ -270,6 +270,11 @@ class DuoquestVerifier:
                 print('Prune: subquery cannot have more than 1 ORDER BY column.')
             return Tribool(False)
 
+        if pred.subquery.has_order_by and not pred.subquery.has_limit:
+            if self.debug:
+                print('Prune: subquery cannot have ORDER BY without LIMIT.')
+            return Tribool(False)
+
         subq = self.prune_by_semantics(schema, pred.subquery)
         if subq is not None:
             return subq
