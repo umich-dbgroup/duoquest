b49645215f6a888eb6fb39f7fe834a4d9370ddaf
Chris Baik <cjbaik@umich.edu>
2019-07-17 20:58:27 -0400

    fix orderby/limit check

--- duoquest/verifier.py
+++ duoquest/verifier.py
@@ -270,8 +270,8 @@ class DuoquestVerifier:
                 print('Prune: subquery cannot have more than 1 ORDER BY column.')
             return Tribool(False)
 
-        if pred.subquery.done_order_by and pred.subquery.has_order_by and \
-            not pred.subquery.has_limit:
+        if pred.subquery.has_order_by == TRUE and \
+            pred.subquery.has_limit == FALSE:
             if self.debug:
                 print('Prune: subquery cannot have ORDER BY without LIMIT.')
             return Tribool(False)
