25d2ae35bce8361ee3f1b5dbbca2d108832920d1
Chris Baik <cjbaik@umich.edu>
2019-07-22 18:15:49 -0400

    fix b

--- duoquest/files.py
+++ duoquest/files.py
@@ -1,6 +1,6 @@
 import os
 
 def results_path(config, system, dataset, mode, n, tsq_level, tsq_rows, cache):
-    basename = '_'.join([system, dataset, mode, f'n{n}', f'b{b}', tsq_level,
+    basename = '_'.join([system, dataset, mode, f'n{n}', tsq_level,
         f'r{tsq_rows}', f'c{int(cache)}'])
     return os.path.join(config['duoquest']['results_dir'], f'{basename}')
