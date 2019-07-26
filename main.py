692a27e3fa2d974c5c95a5621f4b64746730ff9b
Chris Baik <cjbaik@umich.edu>
2019-07-26 09:42:13 -0400

    default timeout 60

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
     # NLQ parameters
     # parser.add_argument('--n', default=10, type=int,
     #     help='Max number of final queries to output')
-    parser.add_argument('--timeout', default=5, type=int,
+    parser.add_argument('--timeout', default=60, type=int,
         help='Timeout if search does not terminate')
 
     parser.add_argument('--tid', default=None, type=int, help='debug task id')
