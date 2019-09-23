e4e7d745c0788af57877017a118c0eb24e2130cc
Chris Baik <cjbaik@umich.edu>
2019-09-23 18:13:28 -0400

    fix debug stmt

--- duoquest/verifier.py
+++ duoquest/verifier.py
@@ -544,7 +544,7 @@ class DuoquestVerifier:
         for literal in literals.num_lits:
             if not self.find_literal_usage(query, literal):
                 if self.debug:
-                    print(f'Prune: No literal {literal.col_id}:{literal.value}')
+                    print(f'Prune: No literal {literal.value}')
                 return Tribool(False)
         return None
 
