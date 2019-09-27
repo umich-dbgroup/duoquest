1bdeac5943b305446a5ebea00fa82d722558d7a3
Chris Baik <cjbaik@umich.edu>
2019-09-27 17:07:40 -0400

    move hack up

--- duoquest/server.py
+++ duoquest/server.py
@@ -404,13 +404,13 @@ class DuoquestServer:
                 elif result.value:
                     response.results.append(TRUE)
 
+                    # HACK: but we don't really consider distinct
+                    query.distinct = gold.distinct
+                    
                     print(query)
                     print('--------')
                     print(gold)
 
-                    # HACK: but we don't really consider distinct
-                    query.distinct = gold.distinct
-                    
                     if query.SerializeToString() == gold.SerializeToString():
                     # if eval_gold and eval_kmaps and is_correct(db, schema.db_id,
                     #     eval_kmaps, eval_gold, generate_sql_str(query, schema)):
